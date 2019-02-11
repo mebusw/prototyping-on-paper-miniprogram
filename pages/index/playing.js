47e12b834836ecf24fb14c535a89c2278fef6132
JackyShen <mebusw@163.com>
2019-02-11 18:47:23 +1300

    利用z-index和animation来可视化地交换两张图片，模拟transition转场

--- pages/index/playing.js
+++ pages/index/playing.js
@@ -9,28 +9,33 @@ Page({
     currPicIdx: 0,
     zBufferA: 2000,
     zBufferB: 1000,
-    isAoverB: true
-
+    animationDataA: {},
+    animationDataB: {}
   },
+  isAoverB: true,
+  isAnimating: false, // disable tapping when animating
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this
     wx.getStorage({
       key: 'projects',
       success(res) {
         console.log(res.data)
-        that.setData({ 'projects': res.data })
+        that.setData({
+          'projects': res.data
+        })
       }
     })
-  
-      wx.getSystemInfo({
+
+    wx.getSystemInfo({
       success(res) {
         console.log(res.model)
         console.log(res.pixelRatio)
         console.log(res.windowWidth) //可使用窗口宽度，单位px
+        that.windowWidth = res.windowWidth
         console.log(res.windowHeight)
         console.log(res.language)
         console.log(res.version)
@@ -42,19 +47,67 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     var that = this
-    that.setData({ 'picPathA': this.data.projects[0].pics[this.data.currPicIdx].path, 'picPathB': this.data.projects[0].pics[this.data.currPicIdx + 1].path })
+    that.setData({
+      'picPathA': this.data.projects[0].pics[this.data.currPicIdx].path,
+      'picPathB': this.data.projects[0].pics[this.data.currPicIdx + 1].path
+    })
+
+    this.animation = wx.createAnimation({
+      duration: 800,
+      transformOrigin: "50% 50%",
+      timingFunction: "ease", //timingFunction: 'cubic-bezier(.8,.2,.1,0.8)',
+      delay: 0
+    })
+    this.animationRecover = wx.createAnimation({
+      duration: 10,
+      transformOrigin: "50% 50%",
+      timingFunction: "step-start", //timingFunction: 'cubic-bezier(.8,.2,.1,0.8)',
+      delay: 10
+    })
   },
 
   tap: function(e) {
+    var that = this
+    // if(isAnimating){}
     console.log(e)
-    var tmp = this.data.zBufferA
-    this.setData({ 'zBufferA': this.data.zBufferB, 'zBufferB': tmp})
+    console.log('tap, isAoverB= ' + this.isAoverB)
 
+    this.animation.translateX(-1 * this.windowWidth).step()
+    if (this.isAoverB) {
+      this.setData({
+        animationDataA: this.animation.export()
+      })
+    } else {
+      this.setData({
+        animationDataB: this.animation.export()
+      })
+    }
+
+    //after animation, swap the z-index of buffers
+    setTimeout(function() {
+      this.animationRecover.translateX(0).step()
+      if (this.isAoverB) {
+        that.setData({
+          'zBufferA': 1000,
+          'zBufferB': 2000,
+          animationDataA: this.animationRecover.export()
+        })
+      } else {
+        that.setData({
+          'zBufferA': 2000,
+          'zBufferB': 1000,
+          animationDataB: this.animationRecover.export()
+        })
+      }
+      this.isAoverB = !(this.isAoverB)
+      console.log('timeout, isAoverB= ' + this.isAoverB)
+
+    }.bind(this), 1000)
   },
 
-  longtap: function (e) {
+  longtap: function(e) {
     console.log(e)
     wx.showModal({
       title: '提示',
@@ -68,47 +121,47 @@ Page({
         }
       }
     })
-  },  
-  
+  },
+
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
